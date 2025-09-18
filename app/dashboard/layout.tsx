734fd850a459c4135d6a6a3014a067481f03b13a
qbentil <bentilshadrack72@gmail.com>
2025-09-18 11:15:28 +0000

    Updated the layout resizing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,12 +6,15 @@ import Footer from "@/components/footer/dashboard";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import { usePathname } from "next/navigation";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <AuthGuard>
@@ -20,7 +23,7 @@ export default function DashboardLayout({
           <div className="flex flex-grow">
             <Sidebar />
             {/* main content with 70% */}
-            <div className="flex-1 bg-white flex flex-col">
+            <div className="flex-1 bg-white flex flex-col" key={pathname}>
               <header className="h-16 w-full shadow-md">
                 <Navbar />
               </header>
